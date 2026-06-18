ba452195c25c3332837fca7c64c2c248ca9c70ce
larrybwosi <larrybwosi@gmail.com>
2026-06-18 08:15:58 +0300

    update

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -2,13 +2,16 @@ import { betterAuth } from "better-auth";
 import { authOptions } from "./index";
 import { admin } from "better-auth/plugins";
 import { nextCookies } from "better-auth/next-js";
-import { customSession } from "better-auth/plugins/custom-session";
+import { customSession } from "better-auth/plugins";
 import { UserRole, MemberRole } from "@repo/db";
 import { db } from "@repo/db";
 import { getRedisClient } from "@repo/shared/redis";
 
 export const auth = betterAuth({
   ...(authOptions as any),
+  session: {
+    preserveSessionInDatabase: true,
+  },
   secondaryStorage: {
     get: async (key: string) => {
       try {
