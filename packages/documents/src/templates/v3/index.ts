d780b64573b711583040543501e83a1ec715c252
Larry Dean <143650164+larrybwosi@users.noreply.github.com>
2026-06-29 08:23:06 +0300

    Refactor V3 Invoices and Add V3 Receipts (#398)
    * feat(documents): refactor v3 invoices and add receipt equivalents
    
    - Define streamlined V3DocumentData type
    - Update TemplateOne through TemplateFour to accept dynamic props
    - Add support for 'receipt' type in shared templates
    - Create v3/receipt templates as wrappers
    - Remove IIFE PDF rendering scripts from templates
    - Update document registry and exports
    
    * feat(documents): refactor v3 document templates and register in registry
    
    - Define streamlined V3DocumentData prop type
    - Refactor all 4 V3 invoice templates to be dynamic and support both invoice/receipt types
    - Create 4 corresponding receipt templates as wrappers
    - Remove IIFE rendering scripts and convert to JSX
    - Fix TypeScript strict mode issues (null/undefined checks for optional fields)
    - Register all new V3 templates in the document registry

---
+++ packages/documents/src/templates/v3/index.ts
@@ -0,0 +1,11 @@
+export * from "./invoice/one";
+export * from "./invoice/two";
+export * from "./invoice/three";
+export * from "./invoice/four";
+
+export * from "./receipt/one";
+export * from "./receipt/two";
+export * from "./receipt/three";
+export * from "./receipt/four";
+
+export * from "./types";
