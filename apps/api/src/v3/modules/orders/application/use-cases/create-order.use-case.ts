3c46a54f7266e1d697396b229f68307e21a30413
larrybwosi <larrybwosi@gmail.com>
2026-07-06 07:53:43 +0300

    release

--- apps/api/src/v3/modules/orders/application/use-cases/create-order.use-case.ts
+++ apps/api/src/v3/modules/orders/application/use-cases/create-order.use-case.ts
@@ -8,8 +8,8 @@ import { IOrderRepository } from "../../domain/repositories/order-repository.int
 import { CreateOrderDto } from "../dto/create-order.dto";
 import { PrismaService } from "@/prisma/prisma.service";
 import { WebhookService } from "../../../webhooks/infrastructure/services/webhook.service";
-import { ApiRealtimeService } from "../../../../../common/services/realtime.service";
-import { ScrymeNotificationService } from "../../../../../v2/scryme/scryme-notification.service";
+import { ApiRealtimeService } from "@/common/services/realtime.service";
+import { ScrymeNotificationService } from "@/v2/scryme/scryme-notification.service";
 import { emitOrderPlaced } from "@repo/windmill/server";
 import { createOrder } from "@repo/shared/actions";
 import { type CreateOrderInput } from "@repo/shared/lib";
@@ -66,16 +66,14 @@ export class CreateOrderUseCase {
         quantity: Number(i.quantity),
         lineTotal: Number(i.lineTotal),
       })),
-    }).catch((err) =>
+    }).catch(err =>
       console.error("[v3 Order] Failed to emit Windmill event:", err),
     );
 
     // 6. Notify Scryme
     await this.scrymeNotificationService
       .notifyOrderCreated(organizationId, order.id)
-      .catch((err) =>
-        console.error("[v3 Order] Failed to notify Scryme:", err),
-      );
+      .catch(err => console.error("[v3 Order] Failed to notify Scryme:", err));
 
     return order;
   }
