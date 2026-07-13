f4ae5413af6c3be76fa1661479b6f721fa1ce212
larrybwosi <larrybwosi@gmail.com>
2026-07-13 13:33:16 +0300

    portal update

--- apps/portal/app/layout.tsx
+++ apps/portal/app/layout.tsx
@@ -1,2 +1,8 @@
 import "./globals.css";
-export default function Root({children}: {children: React.ReactNode}) { return (<html><body>{children}</body></html>); }
+export default function Root({ children }: { children: React.ReactNode }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
