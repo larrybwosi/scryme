f4dfb23a7d3888ec50e94abebf3f1e0cf097a154
larrybwosi <larrybwosi@gmail.com>
2026-07-16 18:20:46 +0300

    crm error fix

---
+++ apps/crm/lib/providers.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import * as React from "react";
+import { TooltipProvider } from "@repo/ui/components/ui/tooltip";
+import { RealtimeProvider } from "@repo/shared/realtime/client";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { Toaster } from "@repo/ui/components/ui/sonner";
+import { TopLoader } from "../components/top-loader";
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
+  return (
+    <NuqsAdapter>
+      <TooltipProvider>
+        <RealtimeProvider>
+          <TopLoader />
+          {children}
+          <Toaster />
+        </RealtimeProvider>
+      </TooltipProvider>
+    </NuqsAdapter>
+  );
+}
