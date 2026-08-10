11c3316eac9469d9c5ca69286719e5dd041bc881
larrybwosi <larrybwosi@gmail.com>
2026-08-10 12:55:21 +0300

    update

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "prisma/config";
+import { env } from "@repo/env";
 
 export default defineConfig({
   schema: "src/customer-auth/prisma/schema.prisma",
   datasource: {
-    url: process.env.CUSTOMER_DB,
+    url: env.CUSTOMER_DB,
   },
 });
