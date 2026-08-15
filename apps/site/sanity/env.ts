dce744a6306bc8cc949b195a4b6e3242d21e36b9
larrybwosi <larrybwosi@gmail.com>
2026-08-15 10:27:20 +0300

    update

--- apps/site/sanity/env.ts
+++ apps/site/sanity/env.ts
@@ -1,5 +1,3 @@
-import { env } from "@repo/env";
-
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2026-07-15";
 
@@ -7,12 +5,10 @@ export const dataset =
   process.env.NEXT_PUBLIC_SITE_SANITY_DATASET ||
   process.env.NEXT_PUBLIC_SANITY_DATASET ||
   process.env.SANITY_DATASET ||
-  env.NEXT_PUBLIC_SITE_SANITY_DATASET ||
   "production";
 
 export const projectId =
   process.env.NEXT_PUBLIC_SITE_SANITY_PROJECT_ID ||
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
   process.env.SANITY_PROJECT_ID ||
-  env.NEXT_PUBLIC_SITE_SANITY_PROJECT_ID ||
   "ce88cj7n";
