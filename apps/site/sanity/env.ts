268a47ef51c5db98c9fac67aaee5ceb54ee85a3f
Larry Dean <143650164+larrybwosi@users.noreply.github.com>
2026-09-05 08:26:10 +0300

    feat(site): fetch all content from Sanity CMS with real-time live sync (#1336)

--- apps/site/sanity/env.ts
+++ apps/site/sanity/env.ts
@@ -1,14 +1,14 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2026-07-15";
 
-export const dataset ="production"
-  // process.env.NEXT_PUBLIC_SITE_SANITY_DATASET ||
-  // process.env.NEXT_PUBLIC_SANITY_DATASET ||
-  // process.env.SANITY_DATASET ||
-  ;
+export const dataset =
+  process.env.NEXT_PUBLIC_SITE_SANITY_DATASET ||
+  process.env.NEXT_PUBLIC_SANITY_DATASET ||
+  process.env.SANITY_DATASET ||
+  "production";
 
-export const projectId ="ce88cj7n"
-  // process.env.NEXT_PUBLIC_SITE_SANITY_PROJECT_ID ||
-  // process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
-  // process.env.SANITY_PROJECT_ID ||
-  ;
+export const projectId =
+  process.env.NEXT_PUBLIC_SITE_SANITY_PROJECT_ID ||
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
+  process.env.SANITY_PROJECT_ID ||
+  "ce88cj7n";
